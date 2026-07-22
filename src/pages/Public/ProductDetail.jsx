33b73d227423c57d98e557b3d6b315476dccc582
Bonica-rose <eveningstarforever@gmail.com>
2026-07-22 10:07:46 +0530

    resused loding, emptyState, errormessage ui components in Product Detail page

--- src/pages/Public/ProductDetail.jsx
+++ src/pages/Public/ProductDetail.jsx
@@ -1,9 +1,13 @@
-import { useParams } from "react-router-dom";
-useEffect
-import ProductDetails from "../../components/product/ProductDetails";
-import { useDispatch, useSelector } from "react-redux";
-import { getProduct } from "../../features/products/productThunks";
 import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { getProduct } from "../../features/products/productThunks";
+
+import ProductDetails from "../../components/product/ProductDetails";
+import Loading from "../../components/common/Loading";
+import ErrorMessage from "../../components/common/ErrorMessage";
+import EmptyState from "../../components/common/EmptyState";
 
 const ProductDetail = () => {
   const { id } = useParams();
@@ -15,11 +19,29 @@ const ProductDetail = () => {
     dispatch(getProduct(id));
   }, [dispatch, id]);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
-  if (!product) return <p>Product not found.</p>;
+  return (
+    <div className="container mx-auto py-8">
+      <Link
+        to="/products"
+        className="mb-6 inline-block text-blue-600 hover:underline"
+      >
+        ← Back to Products
+      </Link>
 
-  return <ProductDetails product={product} />;
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <ErrorMessage message={error} />
+      ) : !product ? (
+        <EmptyState
+          title="Product Not Found"
+          message="The product you're looking for doesn't exist or has been removed."
+        />
+      ) : (
+        <ProductDetails product={product} />
+      )}
+    </div>
+  );
 };
 
 export default ProductDetail;
